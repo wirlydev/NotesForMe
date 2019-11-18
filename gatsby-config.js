4912fa55974fefbd6ff3334999f765c2a9ef7b3f
wirlydev <wirlydev@gmail.com>
2019-11-17 22:28:24 -0500

    remove some of the default stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Notes for Me`,
+    author: `Wirly`,
+    description: `Stuff that I know I will forget.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `wirlydev`,
     },
   },
   plugins: [
